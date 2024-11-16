c7d86fa7e7df74ea699675bf7f3a1218c9e3c645
harykali <harykali@outlook.com>
2024-11-16 20:53:38 +0800

    Interpreter first version

--- basic.py
+++ basic.py
@@ -148,6 +148,8 @@ class Lexer:
 class NumberNode:
     def __init__(self, tok):
         self.tok = tok
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
 
     def __repr__(self):
         return f'{self.tok}'
@@ -158,6 +160,8 @@ class BinOpNode:
         self.left_node = left_node
         self.op_tok = op_tok
         self.right_node = right_node
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
 
     def __repr__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
@@ -167,6 +171,8 @@ class UnaryOpNode:
     def __init__(self, op_tok, node):
         self.op_tok = op_tok
         self.node = node
+        self.pos_start = node.pos_start
+        self.pos_end = node.pos_end
 
     def __repr__(self):
         return f'({self.op_tok}, {self.node})'
@@ -267,6 +273,36 @@ class Parser:
         return res.success(left)
 
 
+class Number:
+    def __init__(self, value):
+        self.value = value
+        self.set_pos()
+
+    def set_pos(self, pos_start=None, pos_end=None):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        return self
+
+    def added_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value)
+
+    def subbed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value)
+
+    def multed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value * other.value)
+
+    def dived_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value / other.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+
 class Interpreter:
     def visit(self, node):
         method_name = f"visit_{type(node).__name__}"
@@ -277,13 +313,33 @@ class Interpreter:
         raise Exception(f"No visit {type(node).__name__} method defined")
 
     def visit_NumberNode(self, node):
-        print("Found number node!")
+        # print("Found Number Node!")
+        return Number(node.tok.value).set_pos(node.pos_start, node.pos_end)
 
     def visit_BinOpNode(self, node):
-        print("Found BinOp Node!")
+        # print("Found BinOp Node!")
+        left = self.visit(node=node.left_node)
+        right = self.visit(node=node.right_node)
+        if node.op_tok.type == TT_PLUS:
+            res = left.added_to(right)
+
+        if node.op_tok.type == TT_MINUS:
+            res = left.subbed_by(right)
+
+        if node.op_tok.type == TT_MUL:
+            res = left.multed_by(right)
+
+        if node.op_tok.type == TT_DIV:
+            res = left.dived_by(right)
+
+        return res.set_pos(node.pos_start, node.pos_end)
 
     def visit_UnaryOpNode(self, node):
-        print("Found naryOpNode!")
+        # print("Found naryOpNode!")
+        number = self.visit(node.node)
+        if node.op_tok.type == TT_MINUS:
+            number = number.multed_by(Number(-1))
+        return number.set_pos(node.pos_start, node.pos_end)
 
 
 def run(fn, text):
@@ -298,6 +354,6 @@ def run(fn, text):
     if ast.error: return None, ast.error
 
     interpreter = Interpreter()
-    interpreter.visit(ast.node)
+    result = interpreter.visit(ast.node)
 
-    return None, None
+    return result, None
