228476c98c9c8381225d2b0cddd4c0b11800ca7f
harykali <harykali@outlook.com>
2024-11-17 20:32:15 +0800

    Upgraded syntax to make the order of operations clearer

--- basic.py
+++ basic.py
@@ -254,6 +254,9 @@ class Parser:
             ))
         return res
 
+    def power(self):
+        return self.bin_op(self.atom, (TT_POW,), self.factor)
+
     def factor(self):
         res = ParseResult()
         tok = self.current_tok
@@ -264,7 +267,12 @@ class Parser:
             if res.error: return res
             return res.success(UnaryOpNode(tok, factor))
 
-        elif tok.type in (TT_INT, TT_FLOAT):
+        return self.power()
+
+    def atom(self):
+        res = ParseResult()
+        tok = self.current_tok
+        if tok.type in (TT_INT, TT_FLOAT):
             res.register(self.advance())
             return res.success(NumberNode(tok))
 
@@ -281,26 +289,23 @@ class Parser:
                     "Expected ')'"
                 ))
 
-        return res.failure(InvalidSyntaxError(
-            tok.pos_start, tok.pos_end,
-            "Expected int or float"
-        ))
-
     def term(self):
-        return self.bin_op(self.factor, (TT_MUL, TT_DIV, TT_POW, TT_MOD))
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV, TT_MOD))
 
     def expr(self):
         return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
 
-    def bin_op(self, func, ops):
+    def bin_op(self, func_a, ops, func_b=None):
+        if func_b == None:
+            func_b = func_a
         res = ParseResult()
-        left = res.register(func())
+        left = res.register(func_a())
         if res.error: return res
 
         while self.current_tok.type in ops:
             op_tok = self.current_tok
             res.register(self.advance())
-            right = res.register(func())
+            right = res.register(func_b())
             if res.error: return res
             left = BinOpNode(left, op_tok, right)
 
